ed9db65ed736eddaf2f532f4e54627a7bec232ed
Soichiro Kashima <soichiro.kashima@gmail.com>
2019-11-20 11:05:10 +0900

    Add task list controller test

--- controllers/task_test.go
+++ controllers/task_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -47,4 +48,37 @@ func TestTaskCreateValidationError(t *testing.T) {
 	router.ServeHTTP(w, req)
 	assert.Equal(t, 400, w.Code)
 	assert.Equal(t, `{"error":"Key: 'TaskCreateForm.Title' Error:Field validation for 'Title' failed on the 'required' tag"}`, strings.TrimRight(w.Body.String(), "\n"))
+}
+
+func TestTaskList(t *testing.T) {
+	router := gin.Default()
+	c := new(TaskController)
+	gin.SetMode(gin.TestMode)
+	db.InitForTest()
+	db := db.GetDB()
+	db.AutoMigrate(&models.User{}, &models.Task{})
+	db.Create(&models.User{ID: 100, Name: "foo", Password: "$2a$10$FgKFrUubZOpRwPT9D5p9XuOjCYhPv7eCQwzdQKFJWTQsC9tXAuMG2" /* test */, CreatedAt: time.Now()})
+	tasks := []models.Task{
+		models.Task{ID: 200, UserID: 100, Title: "task1", Done: false, Type: 0, CreatedAt: time.Now()},
+		models.Task{ID: 201, UserID: 100, Title: "task2", Done: false, Type: 0, CreatedAt: time.Now()},
+	}
+	for _, v := range tasks {
+		db.Create(&v)
+	}
+	router.Use(func(c *gin.Context) {
+		c.Set("user", 100)
+	})
+	router.GET("/", c.List)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	created := struct {
+		Tasks []models.Task `json:"tasks"`
+	}{}
+	err := json.Unmarshal([]byte(strings.TrimRight(w.Body.String(), "\n")), &created)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(created.Tasks))
+	assert.Equal(t, 200, created.Tasks[0].ID)
+	assert.Equal(t, 201, created.Tasks[1].ID)
 }
