93bffe655a1334c6ed5d286d6c1ca38fd700bb37
Soichiro Kashima <soichiro.kashima@gmail.com>
2019-11-26 10:56:06 +0900

    Add tests for login in models

--- models/user_test.go
+++ models/user_test.go
@@ -34,3 +34,51 @@ func TestNewUser(t *testing.T) {
 	assert.NotEmpty(t, users[0].Password)
 	assert.GreaterOrEqual(t, time.Now().Unix(), users[0].CreatedAt.Unix())
 }
+
+func TestUserLogin(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	config.Init("test")
+	db.Init()
+	db := db.GetDB()
+	db.AutoMigrate(&User{})
+	db.Create(&User{ID: 100, Name: "foo", Password: "$2a$10$FgKFrUubZOpRwPT9D5p9XuOjCYhPv7eCQwzdQKFJWTQsC9tXAuMG2" /* test */, CreatedAt: time.Now()})
+	form := forms.UserLoginForm{
+		Name:     "foo",
+		Password: "test",
+	}
+	token, err := Login(&form)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, "token", token)
+}
+
+func TestUserLoginErrorInvalidName(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	config.Init("test")
+	db.Init()
+	db := db.GetDB()
+	db.AutoMigrate(&User{})
+	db.Create(&User{ID: 100, Name: "foo", Password: "$2a$10$FgKFrUubZOpRwPT9D5p9XuOjCYhPv7eCQwzdQKFJWTQsC9tXAuMG2" /* test */, CreatedAt: time.Now()})
+	form := forms.UserLoginForm{
+		Name:     "bar",
+		Password: "test",
+	}
+	token, err := Login(&form)
+	assert.Equal(t, "invalid name or password", err.Error())
+	assert.Empty(t, token)
+}
+
+func TestUserLoginErrorInvalidPassword(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	config.Init("test")
+	db.Init()
+	db := db.GetDB()
+	db.AutoMigrate(&User{})
+	db.Create(&User{ID: 100, Name: "foo", Password: "$2a$10$FgKFrUubZOpRwPT9D5p9XuOjCYhPv7eCQwzdQKFJWTQsC9tXAuMG2" /* test */, CreatedAt: time.Now()})
+	form := forms.UserLoginForm{
+		Name:     "foo",
+		Password: "wrong",
+	}
+	token, err := Login(&form)
+	assert.Equal(t, "invalid name or password", err.Error())
+	assert.Empty(t, token)
+}
