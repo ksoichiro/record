c8a0baf8e544b12a156214ab45e7170517dc2178
Soichiro Kashima <soichiro.kashima@gmail.com>
2019-12-22 15:46:42 +0900

    Aggregate app main style to scss

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -9,14 +11,3 @@ export default {
   name: 'app'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
